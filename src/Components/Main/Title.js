b01e5a2848724b18f6d6ff37bef371bcf31cb57a
aminabdiii <aminabdiii2006@gmail.com>
2024-10-20 15:51:32 +0330

    edit

--- src/Components/Main/Title.js
+++ src/Components/Main/Title.js
@@ -52,30 +52,11 @@ function Title({ children, setNotes, onOverlay, notes }) {
   function handleAddPost() {
     onOverlay();
   }
-  function handleShowModal() {
-    Swal.fire({
-      confirmButtonText:
-        '<a href="https://dubdev.ir" class="swal2-confirm">رفتن به سایت</a>',
-      title: 'سلام و درود خدمت دوستان عزیز سایت dubdev.ir',
-      text: `
-بنده JS رو از دوره جاوا اسکریپت جوناس با دوبله دوستمون آقای مهرشاد چشم خاوری عزیز آموختم. خیلی عالی بود. و با توجه به رضایتی که از نحوه بیان و دوبله مهرشاد جان داشتم, تصمیم گرفتم دوره ری اکت ایشون رو نیز تهیه کنم. هر دو این دوره ها عالی و پروژه محور هستن.
-مهرشاد جان, ازت ممنونیم بابت دوبله ها.`,
-    }).then((result) => {
-      if (result.isConfirmed) window.location.href = 'https://dubdev.ir';
-    });
-  }
 
   return (
     <div className="flex items-center justify-between flex-wrap px-1 max-sm:gap-y-5 max-sm:justify-center max-sm:flex-col text-center">
       <h1 className="text-3xl px-4">{children}</h1>
       <div className="flex gap-5 flex-wrap justify-center text-center">
-        <button
-          onClick={handleShowModal}
-          title="اضافه کردن یادداشت جدید"
-          className="transition-all duration-300 px-3 py-2 flex items-center rounded-lg bg-violet-500 text-white hover:bg-violet-600 max-sm:px-2 max-sm:py-2 max-sm:text-sm max-xxs:px-1 max-xxs:text-xs h-[48px] max-360:w-[103px] max-360:flex max-360:justify-center">
-          <span>dubdev.ir</span>
-          <i className="bi bi-graph-up-arrow px-1 text-xl"></i>
-        </button>
         <button
           title="اضافه کردن یادداشت جدید"
           onClick={handleAddPost}
